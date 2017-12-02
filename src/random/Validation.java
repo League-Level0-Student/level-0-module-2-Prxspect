8fd5f7fb4680debea98250b55f6892267118351a
not you <league@wts5.attlocal.net>
2017-12-02 12:59:08 -0800

    IntroToRandom, Validation

--- src/random/Validation.java
+++ src/random/Validation.java
@@ -9,19 +9,34 @@ import javax.swing.JOptionPane;
 
 public class Validation {
 	public static void main(String[] args) {
+		String name = JOptionPane.showInputDialog("What is your name?");
+		// 1. OPTIONAL: Watch the first 2 minutes of this movie:
+		// http://vimeo.com/2485018
+		for (int i = 0; i < 10; i++) {
+			int randomNumber = new Random().nextInt(5);
 
-		// 1. OPTIONAL: Watch the first 2 minutes of this movie: http://vimeo.com/2485018
-		
-		int randomNumber = new Random().nextInt(5);
-		
-		// 2. On paper, write all the numbers that get printed when you run this class 10 times
-		
-		System.out.println(randomNumber);
+			// 2. On paper, write all the numbers that get printed when you run this class
+			// 10 times
 
-		// 3. Use each value of randomNumber to give the user a random compliment.
+			System.out.println(randomNumber);
+
+			// 3. Use each value of randomNumber to give the user a random compliment.
+
+			if (randomNumber == 0) {
+				JOptionPane.showMessageDialog(null, "You have a nice smile " + name);
+			} else if (randomNumber == 1) {
+				JOptionPane.showMessageDialog(null, "Your awesome " + name);
+			} else if (randomNumber == 2) {
+				JOptionPane.showMessageDialog(null, "You have nice eyes " + name);
+			} else if (randomNumber == 3) {
+				JOptionPane.showConfirmDialog(null, "Your an amazing person " + name);
+			} else if (randomNumber == 4) {
+				JOptionPane.showMessageDialog(null, "You have a cool personality " + name);
+			}
+		}
 
 		// 4. Repeat all the code above 10 times
-		
+
 		// 5. Find someone to test out your program. They will like it :)
 	}
 }
